c6f0acf082230ec7f0f2405cfbbcb791a6f5ff85
Tacofromhell <waldemarsson@gmail.com>
2019-02-09 15:42:25 +0100

    Lite test

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,22 +1,24 @@
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
 
 public class ChatServer {
 
-    private final static ChatServer server = new ChatServer();
+//    private final static ChatServer server = new ChatServer();
     private final int PORT = 1234;
     private boolean running = true;
-    private List<ChatServerThread> connectedClients = new ArrayList<>();
+    private Hashtable connectedClients = new Hashtable();
 
-    private ChatServer() {
+    public ChatServer() {
         try (
             ServerSocket serverSocket = new ServerSocket(PORT)
         ) {
             while (running) {
-                connectedClients.add(new ChatServerThread(serverSocket.accept()));
-                System.out.println("Connected Clients: " + connectedClients.size());
+                new ChatServerThread(serverSocket.accept(), this);
+
             }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
@@ -25,8 +27,32 @@ public class ChatServer {
         }
     }
 
-    public static ChatServer get(){
-        return server;
+    public  ChatServer get(){
+        return this;
+    }
+
+    public void addConnectedClient(Socket key, DataOutputStream value){
+        this.connectedClients.put(key, value);
+    }
+
+    public Hashtable getConnectedClients(){
+        return this.connectedClients;
+    }
+
+    public Enumeration getOutputStreams(){
+        return connectedClients.elements();
+    }
+
+    public void sendToAll(String msg){
+        for (Enumeration e = getOutputStreams(); e.hasMoreElements(); ) {
+// ... get the output stream ...
+            DataOutputStream dout = (DataOutputStream)e.nextElement();
+// ... and send the message
+            try {
+                dout.writeUTF( msg );
+            } catch( IOException ie ) { System.out.println( ie ); }
+        }
+
     }
 
 
--- src/ChatServerThread.java
+++ src/ChatServerThread.java
@@ -3,20 +3,32 @@ import java.io.*;
 
 public class ChatServerThread extends Thread implements Runnable{
     private Socket socket = null;
+    private ChatServer server;
 
-    public ChatServerThread(Socket socket) {
+    public ChatServerThread(Socket socket, ChatServer server) {
         super("ServerThread");
         this.socket = socket;
+        this.server = server;
         this.start();
     }
 
     public void run() {
         try (
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
         ) {
+
+            server.addConnectedClient(socket, dout);
+            System.out.println("Connected Clients: " + server.getConnectedClients().size());
             String msg;
             while ((msg = in.readLine()) != null) {
+                System.out.println("1: " + msg);
+                dout.writeUTF(msg);
+                System.out.println("2: " + msg);
+                dout.flush();
+                System.out.println("3: " + msg);
+//                server.sendToAll(msg);
                 out.println(msg);
                 if (msg.equals("Bye"))
                     break;
