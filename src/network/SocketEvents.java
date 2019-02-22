f8d58a64ff69910321567aa9f4aacefc952559f5
Aarkan <johanwiren1@gmail.com>
2019-02-22 10:55:32 +0100

    enum to tag data

---
+++ src/network/SocketEvents.java
@@ -0,0 +1,13 @@
+package network;
+
+public enum SocketEvents {
+
+    CLIENT_CONNECT,
+    CLIENT_DISCONNECT,
+    ROOM_CREATED,
+    ROOM_DELETED,
+    ROOM_JOIN,
+    ROOM_LEAVE,
+    USER_NAME_CHANGE,
+    MESSAGE;
+}
