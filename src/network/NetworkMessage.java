2488a61499cfdbb6a133b468d5d13f8ef800a494
Andersclark <anders.clark@gmail.com>
2019-02-22 13:40:47 +0100

    Added abstract class NetworkMessage with subclasses for types of objects in received networkcommunication.

---
+++ src/network/NetworkMessage.java
@@ -0,0 +1,53 @@
+package network;
+
+public abstract class NetworkMessage {
+
+    public static class ClientConnect{
+        String userId;
+
+        public ClientConnect(String userId){
+            this.userId = userId;
+        }
+    }
+
+    public static class ClientDisconnect {
+
+    }
+
+    public static class RoomCreate{
+        String roomName;
+        public RoomCreate(String roomName){
+            this.roomName = roomName;
+        }
+    }
+
+    public static class RoomDelete{
+        String targetRoom;
+        public RoomDelete(String targetRoom){
+            this.targetRoom = targetRoom;
+        }
+    }
+
+    public static class RoomJoin{
+        String targetRoom;
+        public RoomJoin(String targetRoom){
+            this.targetRoom = targetRoom;
+        }
+    }
+
+    public static class RoomLeave{
+        String targetRoom;
+        public RoomLeave(String targetRoom){
+            this.targetRoom = targetRoom;
+        }
+    }
+
+    public static class UserNameChange{
+        String newName;
+        public UserNameChange(String newName){
+            this.newName = newName;
+        }
+
+    }
+
+}//END OF CLASS
